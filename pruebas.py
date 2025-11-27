f3615bd99497da28d95156e759011763060fd0ee
Pelaez <miguelpelaez1707@gmail.com>
2025-11-27 17:23:19 +0100

    cambiando como distinguir las galaxias

---
+++ pruebas.py
@@ -0,0 +1,61 @@
+import pandas as pd
+import tarfile
+import os
+
+# 1. Cargar datos
+df_morph = pd.read_csv('data/morphology_catalogue.csv')
+
+# Filtramos (por ejemplo, cogemos las primeras 5 espirales seguras para probar)
+merging_minor = df_morph[df_morph['merging_major-disturbance_fraction'] > 0.4].head(5)
+
+# 2. Configuración
+tar_path = 'data/cutouts_jpg_gz_arcsinh_vis_y.tar' # El archivo de 3.8GB
+output_dir = 'Imagenes_Galaxias'
+os.makedirs(output_dir, exist_ok=True)
+
+print(f"Abriendo el archivo TAR: {tar_path} (esto puede tardar unos segundos)...")
+
+# 3. Abrir el TAR y buscar las imágenes
+# Usamos 'r|' para leer stream si es muy grande, o 'r' normal.
+with tarfile.open(tar_path, 'r') as tar:
+    for index, row in merging_minor.iterrows():
+        # El CSV nos da una ruta larga: /media/.../102042913_NEG...jpg
+        # Solo nos interesa el nombre del archivo final: 102042913_NEG...jpg
+        full_path = row['jpg_loc_gz_arcsinh_vis_y']
+        if pd.isna(full_path):
+            continue
+            
+        filename = os.path.basename(full_path) # Esto extrae solo el nombre.jpg
+        
+        # A veces dentro del TAR están en carpetas, a veces sueltos. 
+        # Buscamos el archivo dentro del TAR que termine con ese nombre
+        try:
+            # Buscamos el miembro dentro del tar (esto es un truco rápido)
+            # Nota: En archivos TAR muy grandes, buscar uno a uno puede ser lento.
+            # Si sabes la estructura interna exacta es mejor, pero probemos buscando el nombre.
+            
+            # Opción rápida: Asumimos que el nombre en el tar es parecido
+            member = None
+            for m in tar.getmembers():
+                if m.name.endswith(filename):
+                    member = m
+                    break
+            
+            if member:
+                # Extraemos la imagen
+                f = tar.extractfile(member)
+                with open(f"{output_dir}/{filename}", 'wb') as out:
+                    out.write(f.read())
+                print(f"✅ Extraída: {filename}")
+            else:
+                print(f"❌ No encontrada en el TAR: {filename}")
+                
+        except Exception as e:
+            print(f"Error con {filename}: {e}")
+
+print("¡Proceso terminado!")
+
+
+
+
+   
---
+++ pruebas2.py
@@ -0,0 +1,168 @@
+
+import numpy as np
+import pandas as pd
+from sklearn.preprocessing import StandardScaler
+from sklearn.model_selection import train_test_split
+from xgboost import XGBClassifier
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.metrics import f1_score, recall_score, confusion_matrix, roc_curve, roc_auc_score, classification_report, precision_score
+import time
+import matplotlib.pyplot as plt
+
+
+#### DESCARGAMOS LOS DATASETS #####
+
+path_morphology = r"C:/Users/Usuario/Desktop/cositas en python/clasificacion de galaxias/data/morphology_catalogue.csv"
+data_morph = pd.read_csv(path_morphology)
+
+path_physical = r"C:/Users/Usuario/Desktop/cositas en python/clasificacion de galaxias/data/useful_physical_measurements.parquet"
+data_phys = pd.read_parquet(path_physical)
+
+#Tomamos las variables que más nos interesan a la hora de inferir si se están fusionando o no y además, las variables de "merging"
+variables_morph = ['object_id', 'mumax_minus_mag', 'mag_segmentation', 'ellipticity', 'kron_radius', 'smooth-or-featured_smooth_fraction', 'smooth-or-featured_featured-or-disk_fraction', 'cutout_width_arcsec',
+                   'merging_none_fraction', 'merging_minor-disturbance_fraction', 'merging_major-disturbance_fraction', 'merging_merger_fraction']
+variables_phys = ['object_id', 'flux_detection_total', 'concentration', 'gini', 'asymmetry', 'moment_20', 'sersic_sersic_vis_index', 'phz_pp_median_redshift', 'phz_pp_median_stellarmass', 'phz_pp_median_sfr']
+
+data = pd.merge(
+    left= data_phys[variables_phys],
+    right= data_morph[variables_morph],
+    on= 'object_id',
+    how= "inner"
+)
+
+# Existen valores infinitos y NaN, vamos a eliminarlos
+
+data.replace([np.inf, -np.inf], np.nan, inplace=True)
+data = data.dropna()
+
+#print(data.shape) #(361913, 21)
+#print(data.head(6).T)
+
+
+
+####### CREAMOS EL CONJUNTO CON EL QUE INFERIREMOS SI LAS GALAXIAS SE ESTAN FUSIONANDO, (i.e. ELIMINAMOS LAS COLUMNAS "merging") #######
+
+X = data.drop(['merging_none_fraction', 'merging_minor-disturbance_fraction', 'merging_major-disturbance_fraction', 'merging_merger_fraction'], axis=1)
+
+#print(X.shape)
+#print(X.head(5).T)
+
+
+
+###### CREAMOS EL OUTPUT: 1 SI HAY ALGUN INDICIO DE FUSIÓN, 0 DE LO CONTRARIO #######
+
+data['Merging'] = 0
+
+data['Merging'] = (data['merging_none_fraction'] < data['merging_merger_fraction'] + data['merging_major-disturbance_fraction']).astype(int)
+
+y = data['Merging']
+print(y.value_counts())
+
+
+'''
+####################------------------------------------------##################
+
+
+aux = pd.concat([X,y], axis=1)
+
+corr = aux.corr()
+correlaciones = corr.iloc[-1, :].sort_values()
+
+print(correlaciones)
+'''
+
+
+
+
+
+#### PREPARAR LOS DATOS PARA ENTRENAMIENTO Y PRUEBA, ADEMÁS DE ESTANDARIZAR LOS DATOS ##########
+
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, stratify= y, random_state=42)
+
+## Estandarizamos las muestras:
+scaler = StandardScaler()
+
+X_train_scaled = pd.DataFrame(scaler.fit_transform(X_train), columns= X_train.columns, index= X_train.index)
+X_test_scaled = pd.DataFrame(scaler.transform(X_test), columns= X_test.columns, index= X_test.index)
+
+
+######## CREAMOS LOS MODELOS DONDE VAMOS A IMPLEMENTAR LOS DATOS #######
+
+## XGBoosting
+xgb = XGBClassifier(n_estimators=100,
+                    learning_rate=0.05,
+                    max_depth=3,
+                    scale_pos_weight=7, 
+                    objective='binary:logistic',
+                    n_jobs=-1,
+                    eval_metric='auc'
+                    )
+
+
+## Random Forest
+rf = RandomForestClassifier(
+    n_estimators= 200,
+    max_depth= 8,
+    class_weight= 'balanced',
+    n_jobs= -1
+)
+
+
+####### ENTRENAMIENTO Y PREDICIONES DE LOS MODELOS ######
+
+## Entrenamiento
+start_time_xgb = time.time()
+xgb.fit(X_train, y_train)
+
+
+#y_pred_xgb = xgb.predict(X_test_scaled)
+y_probs_xgb = xgb.predict_proba(X_test_scaled)[:, 1] 
+y_pred_xgb_ajusted = (y_probs_xgb >= 0.6).astype(int)
+end_time_xgb = time.time()
+xgb_train_time = end_time_xgb - start_time_xgb
+
+#---------------------------------#
+
+start_time_rf = time.time()
+rf.fit(X_train, y_train)
+
+#y_pred_rf = rf.predict(X_test_scaled)
+y_probs_rf = rf.predict_proba(X_test_scaled)[:, 1] 
+y_pred_rf_ajusted = (y_probs_rf >= 0.5).astype(int)
+end_time_rf = time.time()
+rf_train_time = end_time_rf - start_time_rf
+
+print(f'Tiempo de entrenamiento y predicción de XGBoosting: {xgb_train_time} segundos')
+print(f'Tiempo de entrenamiento y prendicción de Random Forest: {rf_train_time} segundos')
+
+
+######## EVALUACIÓN DE LOS MODELOS ########
+
+print('\n~~~~~~~~~~~~~~ EVALUACION DE XGBoosting ~~~~~~~~~~~~~~')
+print(f"AUC-ROC Score: {roc_auc_score(y_test, y_pred_xgb_ajusted):.4f}")
+print(f'F1-score:  {f1_score(y_test, y_pred_xgb_ajusted):.4f}')
+print(f'Recall:   {recall_score(y_test, y_pred_xgb_ajusted):.4f}')
+print(f'Precision:   {precision_score(y_test, y_pred_xgb_ajusted):.4f}')
+print('Confusion matrix:\n')
+print(confusion_matrix(y_test, y_pred_xgb_ajusted))
+
+print('\n~~~~~~~~~~~~~~ EVALUACION DE Random Forest ~~~~~~~~~~~~~~')
+print(f"AUC-ROC Score: {roc_auc_score(y_test, y_pred_rf_ajusted):.4f}")
+print(f'F1-score:  {f1_score(y_test, y_pred_rf_ajusted):.4f}')
+print(f'Recall:   {recall_score(y_test, y_pred_rf_ajusted):.4f}')
+print(f'Precision:   {precision_score(y_test, y_pred_rf_ajusted):.4f}')
+print('Confusion matrix:\n')
+print(confusion_matrix(y_test, y_pred_rf_ajusted))
+
+
+
+
+
+
+
+
+
+
+
+
+
